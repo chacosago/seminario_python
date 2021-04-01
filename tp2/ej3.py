80ed40b6ed030eb5ecf74686e824139afcd96523
Facundo Sago <chaco_sago@hotmail.com>
2021-03-31 21:07:14 -0300

    agrego el ej 3 resuelto y el link al material

---
+++ tp2/ej3.py
@@ -0,0 +1,32 @@
+#Dado un texto solicite por teclado una letra e imprima las palabras que comienzan con dicha
+#letra. En caso que no se haya inrgesado un letra, indique el error. Ver: módulo string
+import string
+
+texto = """
+        Dado un texto solicite por teclado una letra e imprima las palabras que comienzan con dicha
+        letra. En caso que no se haya inrgesado un letra, indique el error. Ver: módulo string
+        """
+
+palabras = texto.split()
+print("Texto: ", texto)
+print("Palabras: ", palabras)
+
+plabras_elegidas = []
+
+letra = input("\nIngrese letra, se buscaran las palabas que inicien con ella, \"FIN\" para terminar ")
+while letra != "FIN":
+    if letra[0].isalpha() and len(letra) == 1:
+        for palabra in palabras:
+            if (palabra[0] == letra) or (palabra[0] == letra.upper()) or (palabra[0] == letra.lower()):
+                plabras_elegidas.append(palabra)
+        if len(plabras_elegidas) > 0:
+            print(f"Las palabras que inician con la letra \"{letra}\" son: \n {plabras_elegidas}")
+        else:
+            print(f"No hay ninguna palabra que empiece con \"{letra}\"")
+    else:
+        print(f"Error, usted NO ingreso una letra, inrgeso: \"{letra}\", intente nuevamente")
+
+    letra = input("Ingrese letra, se buscaran las palabas que inicien con ella, \"FIN\" para terminar ")
+    plabras_elegidas = []
+    
+
