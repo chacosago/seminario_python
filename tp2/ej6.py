a1ae045d4fe0cc2627f68fec91d1d01c1c0e2702
Facundo Sago <chaco_sago@hotmail.com>
2021-04-03 16:55:15 -0300

    agrego ej6

---
+++ tp2/ej6.py
@@ -0,0 +1,19 @@
+# Dada una frase donde las palabras pueden estar repetidas e indistintamente en mayúsculas y
+# minúsculas, imprimir una lista con todas las palabras sin repetir y en letra minúscula.
+
+from texto_en_palabras import texto_en_palabras 
+
+frase = """
+        Si trabajás mucho CON computadoras, eventualmente encontrarás que te gustaría
+        automatizar alguna tarea. Por ejemplo, podrías desear realizar una búsqueda y
+        reemplazo en un gran número DE archivos de texto, o renombrar y reorganizar
+        un montón de archivos con fotos de una manera compleja. Tal vez quieras
+        escribir alguna pequeña base de datos personalizada, o una aplicación
+        especializada con interfaz gráfica, o UN juego simple.
+        """
+
+palabras = texto_en_palabras(frase.lower())
+print(f"len() de la frase: {len(frase)}\n")
+print(f"len de palabras {len(palabras)}, y palabras:\n {palabras}\n")
+palabras_unicas = list(set(palabras))
+print(f"len de palabras_unicas {len(palabras_unicas)}, y sus palabras:\n {palabras_unicas}")
