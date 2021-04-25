803d90f965328111b85606266bb761f0e62d5413
Facundo Sago <chaco_sago@hotmail.com>
2021-04-25 19:21:57 -0300

    agrego manejo de excepciones para nombre de arhivos y verificacion de nro de opcion

--- otras-pruebas/proyecto-carla/diferencias-auweb-siuv2.py
+++ otras-pruebas/proyecto-carla/diferencias-auweb-siuv2.py
@@ -10,6 +10,7 @@ import sys
 import os
 import openpyxl
 
+# Despues estaria bueno resolver problemas con casos de tildes y falsos positivos:
 #caracteres_tildes = "áàéèíìóòúù"
 #caracteres_tildes_l = list(caracteres_tildes)
 
@@ -17,13 +18,30 @@ import openpyxl
 #os.chdir("/home/hcastorp/Drive/Facultad/Informatica/Python/TP/otras-pruebas/proyecto-carla/")
 print("Tenga en cuenta que los archivos a utilizar deben estar en la misma carpeta que el programa y deben incluir su extensión.\n")
 if len(sys.argv) > 1:
-    archivo_aweb = sys.argv[1]
-    archivo_siu = sys.argv[2]
+    archivo_siu = sys.argv[1]
+    archivo_aweb = sys.argv[2]
 else:
-    archivo_aweb = input("Ingresar nombre del arhivo descargado de AulasWeb: ")
     archivo_siu = input("Ingresar nombre del arhivo descargado de Siu-Guarani: ")
-#excel_siu = xlrd.open_workbook('Inscripciones a cursadas.xls')
-excel_siu = xlrd.open_workbook(archivo_siu)
+    archivo_aweb = input("Ingresar nombre del arhivo descargado de AulasWeb: ")
+while True:
+    try:
+        excel_siu = xlrd.open_workbook(archivo_siu)
+        print("archivo aceptado\n")
+        break
+    except:
+        print("Nombre de arvhivo de Siu-Guarani incorrecto o arhivo no encontrado, volvé a intentar")
+        archivo_siu = input("Ingresar nombre del arhivo descargado de Siu-Guarani: ")
+
+
+while True:
+    try:
+        excel_auweb = openpyxl.load_workbook(archivo_aweb)
+        print("archivo aceptado\n")
+        break
+    except:
+        print("Nombre de arvhivo de AulasWeb incorrecto o arhivo no encontrado, volvé a intentar")
+        archivo_aweb = input("Ingresar nombre del arhivo descargado de AulasWeb: ")
+
 hoja_siu = excel_siu.sheet_by_index(0)
 
 alumnos_siu = []
@@ -36,8 +54,6 @@ for i in range(11,hoja_siu.nrows):
 
 # Ahora con los de AulasWeb
 
-#excel_auweb = openpyxl.load_workbook('TV2_COM2_2021  ELECCION DE GRUPOS.xlsx')
-excel_auweb = openpyxl.load_workbook(archivo_aweb)
 hoja_auweb = excel_auweb.active
 
 alumnos_auweb = []
@@ -47,7 +63,17 @@ for fila in hoja_auweb.iter_rows(values_only=True):
 
 alumnos_no_encontrados = []
 
-opcion = int(input("Ingrese la opcion deseada:\n1 - Para busacar alumnos que estan en Aulas Web pero no en Siu-Guarani.\n2 - Para busacar alumnos que estan en Siu-Guarani pero no en Aulas Web: "))
+while True:
+    try:
+        opcion = int(input("Ingrese la opcion deseada:\n1 - Para busacar alumnos que estan en Aulas Web pero no en Siu-Guarani.\n2 - Para busacar alumnos que estan en Siu-Guarani pero no en Aulas Web: "))
+    except ValueError:
+        print("Tenes que ingresar numero no letras o simbolos")
+    if opcion != 1 and opcion != 2:
+        print("Opcion invalida, volvé a intentar\n")
+        continue
+    else:
+        break
+
 
 if opcion == 1:
     # Resuelvo problema: qué alumnos de la lista de aweb no estan en la lista de siu.
@@ -88,4 +114,4 @@ for i in lista_ordenada:
         print(f'{c:3d} {i["Nombre"].capitalize():<30}{i.get("Apellido").capitalize():<30}{i.get("Legajo"):<10} {i.get("Mail"):<40}')
 
 # para que en Windows no cierre la ventana al terminar la ejecucion
-input("")
+input("\nEnter para salir")
